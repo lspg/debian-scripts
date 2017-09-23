769acc68a6348744710a80deefe51b37b81474aa
Loïs PUIG <lois.puig@kctus.fr>
2017-09-23 15:24:22 +0200

    Add file content check

--- pve-enable-lxc-tuntap.sh
+++ pve-enable-lxc-tuntap.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 read -p "CONTAINER ID ? " CTID
+echo "Creating file : /var/lib/lxc/$CTID/autodev"
 cat <<EOF > /var/lib/lxc/$CTID/autodev
 #!/bin/bash
 cd ${LXC_ROOTFS_MOUNT}/dev
@@ -8,5 +9,20 @@ mknod net/tun c 10 200
 chmod 0666 net/tun
 EOF
 
-echo "lxc.cgroup.devices.allow: c 10:200 rwm" >> /etc/pve/lxc/$CTID.conf
-echo "lxc.hook.autodev: /var/lib/lxc/$CTID/autodev" >> /etc/pve/lxc/$CTID.conf
+STRING="lxc.cgroup.devices.allow: c 10:200 rwm"
+STRINGTEST=$(cat /etc/pve/lxc/${CTID}.conf|grep "${STRING}")
+if [ "$STRINGTEST" == "" ]; then
+	echo "Line added to /etc/pve/lxc/$CTID.conf : ${STRING}"
+	echo ${STRING} >> /etc/pve/lxc/$CTID.conf
+else
+	echo "Line already present in /etc/pve/lxc/$CTID.conf : ${STRING}"
+fi
+
+STRING="lxc.hook.autodev: /var/lib/lxc/${CTID}/autodev"
+STRINGTEST=$(cat /etc/pve/lxc/${CTID}.conf|grep "${STRING}")
+if [ "$STRINGTEST" == "" ]; then
+	echo "Line added to /etc/pve/lxc/$CTID.conf : ${STRING}"
+	echo ${STRING} >> /etc/pve/lxc/$CTID.conf
+else
+	echo "Line already present in /etc/pve/lxc/$CTID.conf : ${STRING}"
+fi
